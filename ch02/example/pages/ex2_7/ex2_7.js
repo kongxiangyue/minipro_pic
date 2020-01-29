d37970e73987f7d1d47c00e6d1215eeadeff9cf9
kongxiangyue <513001363@qq.com>
2020-01-29 10:06:22 +0800

    [A] 添加一些例子

---
+++ ch02/example/pages/ex2_7/ex2_7.js
@@ -0,0 +1,72 @@
+// pages/ex2_7/ex2_7.
+
+Page({
+
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    item:{
+      index:0,
+      msg:"this is template",
+      time:"2018-06-13"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
