98487e08d8d16eab22ea1fad5886b184319b6f18
kongxiangyue <513001363@qq.com>
2020-03-17 17:20:07 +0800

    [A]添加例程

---
+++ example03/pages/ex3_1_4/ex3_1_4.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    x: 0,
+    y: 0
+  },
+  tap: function (e) {
+    this.setData({
+      x: 30,
+      y: 30
+    });
+  },
+  onChange: function (e) {
+    console.log(e.detail)
+  },
+  onScale: function (e) {
+    console.log(e.detail)
+  }
+})
